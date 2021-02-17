8dba3656c4d6b35187fe22d1361d5843eea071ce
Jennifer <jenndeng777@gmail.com>
2021-02-16 17:49:12 -0800

    wrote test 13 for correct password with symbol &*

--- tests.py
+++ tests.py
@@ -102,7 +102,7 @@ class TestCase(unittest.TestCase):
         self.assertFalse(check_pwd(assert_input),
                          msg='check_pwd()'.format())
 
-    def test12(self):
+    def test13(self):
         # test for password with correct characters length of 8
         # Checking to make sure test pass for correct character %^
         assert_input = "abcdfH&*"
