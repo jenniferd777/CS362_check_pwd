84c3b080576e02081a5c9686f1cf42cc18f9669c
Jennifer <jenndeng777@gmail.com>
2021-02-16 17:43:10 -0800

    wrote test 12 for correct password with symbol %^

--- tests.py
+++ tests.py
@@ -95,6 +95,12 @@ class TestCase(unittest.TestCase):
         self.assertFalse(check_pwd(assert_input),
                          msg='check_pwd()'.format())
 
+    def test12(self):
+        # test for password with correct characters length of 8
+        # Checking to make sure test pass for correct character %^
+        assert_input = "abcdfH%^"
+        self.assertFalse(check_pwd(assert_input),
+                         msg='check_pwd()'.format())
 
 if __name__ == '__main__':
     unittest.main()
